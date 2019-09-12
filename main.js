b9bebc942e8549c62fe65701e7efbaf9531f6c31
Danilov Nikolai <mailabc.ru@yandex.ru>
2019-09-12 22:03:16 +0300

    Added new cycles while, do while.

--- main.js
+++ main.js
@@ -24,6 +24,39 @@ for (let i = 0; i < 2; i++) {
         }   
 };
 
+//Cycle while
+// let i = 0;
+// while (i <2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
+//         if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+//             appData.expenses[a] = b;
+//             console.log("ok");
+//         } else {
+//             i--;
+//             console.log("not ok");
+//         }
+//     i++;
+//     console.log("_+++");
+// };
+
+//cycle do while
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
+//         if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+//             appData.expenses[a] = b;
+//             console.log("ok");
+//         } else {
+//             i--;
+//             console.log("not ok");
+//         }
+//     i++;
+//     console.log("_+++");
+// }
+// while (i < 2);
+
 alert(appData.budget / 30);
 console.log(appData);
 
