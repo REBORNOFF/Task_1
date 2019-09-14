c2dcb07f6bd0b7dccee063c0392d4c2ea24dd716
Danilov Nikolai <mailabc.ru@yandex.ru>
2019-09-14 22:31:22 +0300

    added new functions

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 "use strict";
 
 //Создание переменных
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let money, time;
 
 //Создание объекта appData
 let appData = {
@@ -11,18 +10,69 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt("Во сколько обойдется?", "");
+function start () {
+    money = +prompt("Ваш бюджет на месяц?", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+};
+
+start();
+
+//Статья обязательных расходов
+function chooseExpenses () {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
+           
         if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+            console.log("Ok");
             appData.expenses[a] = b;
         } else {
             i--;
         }   
+    }
 };
+
+chooseExpenses();
+
+//Ежедневный бюджет
+function detectDayBudget () {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);
+    console.log(appData);
+};
+
+detectDayBudget();
+
+//Сумма накоплений
+function checkSavings () {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+};
+
+checkSavings();
+
+//Необязательные расходы 
+function chooseOptExpenses () {
+    for (let i = 1; i <= 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i] = questionOptExpenses;
+        console.log(appData.optionalExpenses);
+    }
+};
+
+chooseOptExpenses ();
 
 //Cycle while
 // let i = 0;
@@ -56,7 +106,3 @@ for (let i = 0; i < 2; i++) {
 //     console.log("_+++");
 // }
 // while (i < 2);
-
-alert(appData.budget / 30);
-console.log(appData);
-
